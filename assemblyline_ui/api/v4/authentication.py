597522f27682b3aa3dcc32946d460bdb3753beae
L. Lambert <llambert@fbi.gov>
2023-09-26 12:53:07 -0400

    moving around saml stuff

--- assemblyline_ui/api/v4/authentication.py
+++ assemblyline_ui/api/v4/authentication.py
@@ -35,11 +35,8 @@ from assemblyline_ui.http_exceptions import AuthenticationException
 from assemblyline_ui.security.authenticator import default_authenticator
 from authlib.integrations.base_client import OAuthError
 from authlib.integrations.requests_client import OAuth2Session
-from flask import current_app, make_response, redirect, request
-from flask import session
+from flask import current_app, redirect, request
 from flask import session as flsk_session
-from onelogin.saml2.auth import OneLogin_Saml2_Auth
-from onelogin.saml2.utils import OneLogin_Saml2_Utils
 from passlib.hash import bcrypt
 from werkzeug.exceptions import BadRequest, UnsupportedMediaType
 
@@ -327,38 +324,20 @@ def get_reset_link(**_):
     return make_api_response({"success": False}, "We have no record of this email address in our system.", 400)
 
 
-def init_saml_auth(req):
-    auth = OneLogin_Saml2_Auth(req, custom_base_path=config.auth.saml.path)
-    return auth
+# @auth_api.route('/metadata/')
+# def metadata():
+#     req = prepare_flask_request(request)
+#     auth = OneLogin_Saml2_Auth(req, custom_base_path=config.auth.saml.path)
+#     settings = auth.get_settings()
+#     metadata = settings.get_sp_metadata()
+#     errors = settings.validate_metadata(metadata)
 
-
-@auth_api.route('/metadata/')
-def metadata():
-    req = prepare_flask_request(request)
-    auth = init_saml_auth(req)
-    settings = auth.get_settings()
-    metadata = settings.get_sp_metadata()
-    errors = settings.validate_metadata(metadata)
-
-    if len(errors) == 0:
-        resp = make_response(metadata, 200)
-        resp.headers['Content-Type'] = 'text/xml'
-    else:
-        resp = make_response(', '.join(errors), 500)
-    return resp
-
-
-def prepare_flask_request(request):
-    # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
-    return {
-        'https': 'on' if request.scheme == 'https' else 'off',
-        'http_host': request.host,
-        'script_name': request.path,
-        'get_data': request.args.copy(),
-        # Uncomment if using ADFS as IdP, https://github.com/onelogin/python-saml/pull/144
-        # 'lowercase_urlencoding': True,
-        'post_data': request.form.copy()
-    }
+#     if len(errors) == 0:
+#         resp = make_response(metadata, 200)
+#         resp.headers['Content-Type'] = 'text/xml'
+#     else:
+#         resp = make_response(', '.join(errors), 500)
+#     return resp
 
 
 # noinspection PyBroadException,PyPropertyAccess
@@ -404,97 +383,7 @@ def login(**_):
     oauth_token = data.get('oauth_token', None)
 
     if config.auth.saml.enabled:
-        req = prepare_flask_request(request)
-        auth = OneLogin_Saml2_Auth(req, custom_base_path=config.auth.saml.path)
-        errors = []
-        error_reason = None
-        not_auth_warn = False
-        success_slo = False
-        attributes = False
-        paint_logout = False
-
-        if 'sso' in request.args:
-            return redirect(auth.login())
-            # If AuthNRequest ID need to be stored in order to later validate it, do instead
-            # sso_built_url = auth.login()
-            # request.session['AuthNRequestID'] = auth.get_last_request_id()
-            # return redirect(sso_built_url)
-        elif 'sso2' in request.args:
-            return_to = '%sattrs/' % request.host_url
-            return redirect(auth.login(return_to))
-        elif 'slo' in request.args:
-            name_id = session_index = name_id_format = name_id_nq = name_id_spnq = None
-            if 'samlNameId' in session:
-                name_id = session['samlNameId']
-            if 'samlSessionIndex' in session:
-                session_index = session['samlSessionIndex']
-            if 'samlNameIdFormat' in session:
-                name_id_format = session['samlNameIdFormat']
-            if 'samlNameIdNameQualifier' in session:
-                name_id_nq = session['samlNameIdNameQualifier']
-            if 'samlNameIdSPNameQualifier' in session:
-                name_id_spnq = session['samlNameIdSPNameQualifier']
-
-            return redirect(auth.logout(
-                name_id=name_id, session_index=session_index, nq=name_id_nq,
-                name_id_format=name_id_format, spnq=name_id_spnq))
-        elif 'acs' in request.args:
-            request_id = None
-            if 'AuthNRequestID' in session:
-                request_id = session['AuthNRequestID']
-
-            auth.process_response(request_id=request_id)
-            errors = auth.get_errors()
-            not_auth_warn = not auth.is_authenticated()
-            if len(errors) == 0:
-                if 'AuthNRequestID' in session:
-                    del session['AuthNRequestID']
-                session['samlUserdata'] = auth.get_attributes()
-                session['samlNameId'] = auth.get_nameid()
-                session['samlNameIdFormat'] = auth.get_nameid_format()
-                session['samlNameIdNameQualifier'] = auth.get_nameid_nq()
-                session['samlNameIdSPNameQualifier'] = auth.get_nameid_spnq()
-                session['samlSessionIndex'] = auth.get_session_index()
-                self_url = OneLogin_Saml2_Utils.get_self_url(req)
-                if 'RelayState' in request.form and self_url != request.form['RelayState']:
-                    # To avoid 'Open Redirect' attacks, before execute the redirection confirm
-                    # the value of the request.form['RelayState'] is a trusted URL.
-                    return redirect(auth.redirect_to(request.form['RelayState']))
-            elif auth.get_settings().is_debug_active():
-                error_reason = auth.get_last_error_reason()
-        elif 'sls' in request.args:
-            request_id = None
-            if 'LogoutRequestID' in session:
-                request_id = session['LogoutRequestID']
-
-            def dscb():
-                return session.clear()
-
-            url = auth.process_slo(request_id=request_id, delete_session_cb=dscb)
-            errors = auth.get_errors()
-            if len(errors) == 0:
-                if url is not None:
-                    # To avoid 'Open Redirect' attacks, before execute the redirection confirm
-                    # the value of the url is a trusted URL.
-                    return redirect(url)
-                else:
-                    success_slo = True
-            elif auth.get_settings().is_debug_active():
-                error_reason = auth.get_last_error_reason()
-
-        if 'samlUserdata' in session:
-            paint_logout = True
-            if len(session['samlUserdata']) > 0:
-                attributes = session['samlUserdata'].items()
-
-        # TODO
-        if False:
-            return (errors,
-                    error_reason,
-                    not_auth_warn,
-                    success_slo,
-                    attributes,
-                    paint_logout)
+        pass
 
     if config.auth.oauth.enabled and oauth_provider and oauth_token is None:
         oauth = current_app.extensions.get('authlib.integrations.flask_client')
---
+++ assemblyline_ui/security/saml/saml_auth.py
@@ -0,0 +1,111 @@
+from typing import Any, Dict, Optional
+
+from assemblyline_ui.config import LOGGER, config
+from flask import redirect, request, session
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from onelogin.saml2.utils import OneLogin_Saml2_Utils
+from werkzeug.wrappers import Request, Response
+
+
+def saml_login(use_auth_n_request: bool = False) -> Response:
+    auth: OneLogin_Saml2_Auth = _make_saml_auth()
+
+    sso_built_url: str = auth.login()
+    if use_auth_n_request:
+        request.session["AuthNRequestID"] = auth.get_last_request_id()
+    return redirect(sso_built_url)
+
+
+def saml_logout() -> Response:
+    # SAML SLO
+    auth: OneLogin_Saml2_Auth = _make_saml_auth()
+    return redirect(auth.logout(name_id=session.get('samlNameId'),
+                                session_index=session.get('samlSessionIndex'),
+                                nq=session.get('samlNameIdNameQualifier'),
+                                name_id_format=session.get('samlNameIdFormat'),
+                                spnq=session.get('samlNameIdSPNameQualifier')))
+
+
+def saml_single_logout() -> Optional[Response]:
+    # SAML SLS
+    auth: OneLogin_Saml2_Auth = _make_saml_auth()
+    request_id: str = session.get('LogoutRequestID')
+
+    url: str = auth.process_slo(request_id=request_id,
+                                delete_session_cb=lambda: session.clear())
+
+    errors: list = auth.get_errors()
+
+    if len(errors) == 0:
+        if url:
+            # To avoid 'Open Redirect' attacks, before execute the redirection confirm
+            # the value of the url is a trusted URL.
+            return redirect(url)
+    else:
+        errors = [f" - {error}\n" for error in errors]
+        LOGGER.error(f"SAML SLO request failed: {auth.get_last_error_reason()}\n{''.join(errors)}")
+
+
+# if 'samlUserdata' in session:
+#     paint_logout = True
+#     if len(session['samlUserdata']) > 0:
+#         attributes = session['samlUserdata'].items()
+
+
+def saml_process_assertion() -> Response:
+    '''
+    A SAML Assertion Consumer Service (ACS) is a web service endpoint that is
+    used in the SAML authentication and authorization protocol. The ACS is a
+    service provided by the service provider (SP) that receives and processes
+    SAML assertions from the identity provider (IdP). The ACS is responsible
+    for extracting the relevant information from the SAML assertion, such as
+    the user's attributes or the authentication event, and using that
+    information to grant the user access to the protected resource.
+    '''
+    request_data: Dict[str, Any] = _prepare_flask_request(request)
+    auth: OneLogin_Saml2_Auth = _make_saml_auth(request_data)
+    request_id: str = session.get("AuthNRequestID")
+
+    auth.process_response(request_id=request_id)
+    errors: list = auth.get_errors()
+
+    # not_auth_warn = not auth.is_authenticated()
+    if len(errors) == 0:
+        if "AuthNRequestID" in session:
+            del session["AuthNRequestID"]
+
+        session["samlUserdata"] = auth.get_attributes()
+        session["samlNameId"] = auth.get_nameid()
+        session["samlNameIdFormat"] = auth.get_nameid_format()
+        session["samlNameIdNameQualifier"] = auth.get_nameid_nq()
+        session["samlNameIdSPNameQualifier"] = auth.get_nameid_spnq()
+        session["samlSessionIndex"] = auth.get_session_index()
+
+        self_url = OneLogin_Saml2_Utils.get_self_url(request_data)
+
+        if "RelayState" in request.form and self_url != request.form["RelayState"]:
+            # To avoid 'Open Redirect' attacks, before execute the redirection confirm
+            # the value of the request.form["RelayState"] is a trusted URL.
+            return redirect(auth.redirect_to(request.form["RelayState"]))
+    else:
+        errors = [f" - {error}\n" for error in errors]
+        LOGGER.error(f"SAML SLO request failed: {auth.get_last_error_reason()}\n{''.join(errors)}")
+
+
+def _prepare_flask_request(request: Request) -> Dict[str, Any]:
+    # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
+    return {
+        "https": "on" if request.scheme == "https" else "off",
+        "http_host": request.host,
+        "script_name": request.path,
+        "get_data": request.args.copy(),
+        # Uncomment if using ADFS as IdP, https://github.com/onelogin/python-saml/pull/144
+        # "lowercase_urlencoding": True,
+        "post_data": request.form.copy()
+    }
+
+
+def _make_saml_auth(request_data: Dict[str, Any] = None) -> OneLogin_Saml2_Auth:
+    request_data: Dict[str, Any] = request_data or _prepare_flask_request(request)
+    return OneLogin_Saml2_Auth(request_data,
+                               custom_base_path=config.auth.saml.path)
